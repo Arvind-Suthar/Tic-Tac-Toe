4f04b10a810821b4da8a7c2f1ea38d9d39e123bd
Arvind Suthar <35096203+Arvind-Suthar@users.noreply.github.com>
2024-12-21 12:15:36 +0530

    Added optimization using alpha-beta

--- tic tac toe 2/js/main.js
+++ tic tac toe 2/js/main.js
@@ -1,103 +1,91 @@
-
 //Store the board position as an array
-var board = ["X","","","","","","","",""];
-
-
+let board = ["X", "", "", "", "", "", "", "", ""];
+let timeTaken;
 //Play a move when user clicks on a square
-$(".col").click(function(){
-    var clickSquare = $(this).attr("id");
-    clickSquare = parseInt(clickSquare);
+$(".col").click(function () {
+  let clickSquare = $(this).attr("id");
+  clickSquare = parseInt(clickSquare);
 
+  //Check if position is empty
+  if (board[clickSquare] == "") {
+    //Update board for the move played
+    board[clickSquare] = "O";
 
-    //Check if position is empty
-    if(board[clickSquare] == ""){
+    //Update DOM
+    $(this).html("O");
 
-        //Update board for the move played
-        board[clickSquare] = "O";
+    //Check if position is terminal
+    update(checkWin(board));
 
-        //Update DOM
-        $(this).html("O");
-
-        //Check if position is terminal
-        update(checkWin(board));
-        //checkWin(board);
-
-        //if human isnt winner or game is not drawn then call the AI player to play
-        if(checkWin(board) != "O" && checkWin(board) != 0){
-            computerMove(board);
-        }
+    //if human isnt winner or game is not drawn then call the AI player to play
+    if (checkWin(board) != "O" && checkWin(board) != 0) {
+      computerMove(board);
     }
+  }
 });
-
-
 
 //Description: Loops through the possible and gets score by minimax function
 //             Plays the move with the best score
 //@Params: Array containing values of squares on board
 const computerMove = (board) => {
+  //Get all possible moves in current board
+  let allPossibleMoves = getPossibleMoves(board);
 
-    //Get all possible moves in current board
-    let allPossibleMoves = getPossibleMoves(board);
+  //initialize best score with a very low value, not possible in-game
+  let positionScore = 9999;
 
-    //initialize best score with a very low value, not possible in-game
-    let positionScore = 9999;
+  //initialize best move with next available spot (it will be overwritten)
+  let bestMove = allPossibleMoves[0];
 
-    //initialize best move with next available spot (it will be overwritten)
-    let bestMove = allPossibleMoves[0];
+  //loop through the possible move and calculate the score by minimax
+  allPossibleMoves.forEach((elem) => {
+    board[elem] = "X";
+    const startTime = performance.now();
+    let score = minimax(board, true, 0, -positionScore, positionScore);
+    const endTime = performance.now();
+    timeTaken = endTime - startTime;
+    board[elem] = "";
+    if (score < positionScore) {
+      positionScore = score;
+      bestMove = elem;
+    }
+  });
 
-    //loop through the possible move and calculate the score by minimax
-    allPossibleMoves.forEach(elem => {
-        board[elem] = "X";
-        let score = minimax(board, true, 0);
-        board[elem] = "";
-        if(score < positionScore){
-            positionScore = score;
-            bestMove = elem;
-        }
-    });
-
-    //play the move with the best score
-    board[bestMove] = "X";
-    $("#" + bestMove).html("X");
-    //change req
-    update(checkWin(board));
-}
-
-
+  //play the move with the best score
+  board[bestMove] = "X";
+  $("#" + bestMove).html("X");
+  //change req
+  update(checkWin(board));
+};
 
 //Description: Returns all the possible moves in the current board position
 //@Params: Array containing values of squares on board
 const getPossibleMoves = (board) => {
+  //initialize an empty array
+  let emptySquareArray = [];
 
-    //initialize an empty array
-    let emptySquareArray = [];
-
-    //Loop through all square to check if they are available
-    //Add them to array if they are available
-    for(let i = 0; i < board.length; i++){
-        if(board[i] == ""){
-            emptySquareArray.push(i);
-        }
+  //Loop through all square to check if they are available
+  //Add them to array if they are available
+  for (let i = 0; i < board.length; i++) {
+    if (board[i] == "") {
+      emptySquareArray.push(i);
     }
+  }
 
-    //return the array of possible moves
-    return emptySquareArray;
+  //return the array of possible moves
+  return emptySquareArray;
+};
+
+function update(winner) {
+  //Give a message if any player has won or it is a draw
+  if (winner === "X") {
+    $(".text").html("AI won the game!");
+  } else if (winner === "O") {
+    $(".text").html("Human player won the game!");
+  } else if (winner === 0) {
+    $(".text").html("It is a draw");
+  } else {
+    $(".text").html("Your turn");
+    console.log("Time taken for the search: " + timeTaken);
+  }
 }
-
-
-
-function update(winner){
-        //Give a message if any player has won or it is a draw
-        if(winner === "X"){
-            $(".text").html("You won the game!");
-        }else if(winner === "O"){
-            $(".text").html("AI player won the game!");
-        }else if(winner === 0){
-            $(".text").html("It is a draw");
-        }else{
-            $(".text").html("Your turn");
-        }
-}
-
-
-//computerMove(board);
--- tic tac toe 2/js/minimax.js
+++ tic tac toe 2/js/minimax.js
@@ -1,66 +1,78 @@
-
-
-const minimax = (board, maximising, depth) => {
-    if(isTerminalState(board)){
-        if(checkWin(board) == "X"){
-            return -10;
-        }else if(checkWin(board) == "O"){
-            return 10;
-        }else if(checkWin(board) == 0){
-            return 0;
-        }else{
-            console.log("Not terminal");
-        }
-    }else{
-        if(maximising){
-            //Get all possible moves in current board
-            let allPossibleMoves = getPossibleMoves(board);
-    
-            //initialize best score with a very low value, not possible in-game
-            let positionScore = -999;
-    
-            //loop through the possible move and calculate the score by minimax
-            allPossibleMoves.forEach(elem => {
-                board[elem] = "O";
-                score = minimax(board, false, depth++);
-                board[elem] = "";
-
-                positionScore = Math.max(positionScore, score);
-            });
-            return positionScore;
-        }
-
-        if(!maximising){
-            //Get all possible moves in current board
-            let allPossibleMoves = getPossibleMoves(board);
-    
-            //initialize best score with a very low value, not possible in-game
-            let positionScore = 999;
-    
-            //initialize best move with next available spot (it will be overwritten)
-            //let bestMove = allPossibleMoves[0];
-    
-            //loop through the possible move and calculate the score by minimax
-            allPossibleMoves.forEach(elem => {
-                board[elem] = "X";
-                score = minimax(board, true, depth++);
-                board[elem] = "";
-                positionScore = Math.min(positionScore, score);
-            });
-            return positionScore;
-        }
+const minimax = (board, maximising, depth, alpha, beta) => {
+  if (isTerminalState(board)) {
+    if (checkWin(board) == "X") {
+      return -10;
+    } else if (checkWin(board) == "O") {
+      return 10;
+    } else if (checkWin(board) == 0) {
+      return 0;
+    } else {
+      console.log("Not terminal");
     }
-}
+  } else {
+    if (maximising) {
+      //Get all possible moves in current board
+      let allPossibleMoves = getPossibleMoves(board);
 
+      //initialize best score with a very low value, not possible in-game
+      let positionScore = -Infinity;
 
+      //loop through the possible move and calculate the score by minimax
+      for (let i = 0; i < allPossibleMoves.length; i++) {
+        let elem = allPossibleMoves[i];
+
+        //calculate score of the possible moves with recursion
+        board[elem] = "O";
+        let score = minimax(board, false, depth + 1, alpha, beta);
+        board[elem] = "";
+
+        //update the best score guaranteed
+        positionScore = Math.max(positionScore, score);
+        alpha = Math.max(alpha, positionScore);
+        if (beta <= alpha) {
+          break; //beta cutoff
+        }
+      }
+      return positionScore;
+    }
+
+    if (!maximising) {
+      //Get all possible moves in current board
+      let allPossibleMoves = getPossibleMoves(board);
+
+      //initialize best score with a very low value, not possible in-game
+      let positionScore = Infinity;
+
+      //loop through the possible move and calculate the score by minimax
+      for (let i = 0; i < allPossibleMoves.length; i++) {
+        let elem = allPossibleMoves[i];
+
+        //calculate score of the possible moves with recursion
+        board[elem] = "X";
+        let score = minimax(board, true, depth + 1, alpha, beta);
+        board[elem] = "";
+
+        //update the best score guaranteed
+        positionScore = Math.min(positionScore, score);
+        beta = Math.min(beta, positionScore);
+        if (beta <= alpha) {
+          break; //alpha cutoff
+        }
+      }
+      return positionScore;
+    }
+  }
+};
+
+//check if the current position is a terminal state of the game
 const isTerminalState = (board) => {
-    let winningPlayer = checkWin(board);
+  let winningPlayer = checkWin(board);
 
-    if(winningPlayer == "none"){
-        $(".text").html("Your turn");
-        return false;
-    }else{
-        $(".text").html("Your turn");
-        return true;
-    }
-}
+  if (winningPlayer == "none") {
+    $(".text").html("Your turn");
+    return false;
+  } else {
+    $(".text").html("Your turn");
+    return true;
+  }
+};
